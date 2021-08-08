ec09a9418f9349cf9da3235c8a0d9b78465a6122
= <rragtagbates@gmail.com>
2021-08-08 11:40:59 -0400

    a

--- Loader.lua
+++ Loader.lua
@@ -6,12 +6,7 @@ function DUI.load(name)
     return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/master/" .. name .. ".lua"))()
 end
 
-function DUI.Draw(itemname)
-    local item = Drawing.new(itemname)
-	table.insert(AllControls, item)
- 	return item
-end
-
 getgenv().DUI = DUI
 
+DUI.load("Main/test")
 DUI.load("Main/Base")
--- Main/Cursor.lua
+++ Main/Cursor.lua
@@ -1,8 +1,8 @@
 print("Cursor loaded")
 
-DUI.Cursor = DUI.Draw("Square") --Will make a triangle later
-DUI.Cursor.Filled = true
-DUI.Cursor.Size = Vector2.new(10,10)
-DUI.Cursor.Visible = true
-DUI.Cursor.Color = Color3.fromRGB(85, 85, 85)
-DUI.Cursor.Position = Vector2.new(mouse.X, mouse.Y)
+local Cursor = Drawing.new("Square") --Will make a triangle later
+Cursor.Filled = true
+Cursor.Size = Vector2.new(10,10)
+Cursor.Visible = true
+Cursor.Color = Color3.fromRGB(85, 85, 85)
+Cursor.Position = Vector2.new(mouse.X, mouse.Y)
