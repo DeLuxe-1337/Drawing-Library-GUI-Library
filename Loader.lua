fa19be6e6bc1a23824826a59dc81cbe9eb3504fe
= <rragtagbates@gmail.com>
2021-08-08 11:36:18 -0400

    a

--- Loader.lua
+++ Loader.lua
@@ -6,6 +6,12 @@ function DUI.load(name)
     return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/master/" .. name .. ".lua"))()
 end
 
+function DUI.Draw(itemname)
+    local item = Drawing.new(itemname)
+	table.insert(AllControls, item)
+ 	return item
+end
+
 getgenv().DUI = DUI
 
 DUI.load("Main/Base")
--- Main/Base.lua
+++ Main/Base.lua
@@ -4,10 +4,4 @@ player = game.Players.LocalPlayer
 mouse = player:GetMouse()
 RunService = game:GetService("RunService")
 
---[[function DUI.Draw(itemname)
-    local item = Drawing.new(itemname)
-	table.insert(AllControls, item)
- 	return item
-end]]
-
 DUI.Load("Main/Cursor")
