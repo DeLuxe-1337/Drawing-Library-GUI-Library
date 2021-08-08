a3b92b2e5506c69b23487c3c4c00e1cecdfa8764
= <rragtagbates@gmail.com>
2021-08-08 11:27:42 -0400

    a

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,7 @@ print"Made by DeLuxe"
 DUI = {}
 
 function DUI.load(name)
-    return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/main/" .. name .. ".lua"))()
+    return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/master/" .. name .. ".lua"))()
 end
 
 DUI.load("Main/Base")
