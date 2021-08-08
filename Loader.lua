0dad40122b040344d8d66f4ad89598fdeabc596e
= <74394136+DeLuxe1337001@users.noreply.github.com>
2021-08-08 17:22:53 -0400

    Fixed minor issue

--- Loader.lua
+++ Loader.lua
@@ -6,7 +6,7 @@ function DUI.load(name)
     return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/master/" .. name .. ".lua"))()
 end
 
-local genv = assert(getgenv, "oop")
+local genv = assert(getgenv, "oop")()
 
 genv.DUI = DUI
 
