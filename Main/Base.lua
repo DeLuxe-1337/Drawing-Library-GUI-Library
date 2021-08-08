e4d5db0fc174d61bcb1a9985d21c053585064cd8
= <rragtagbates@gmail.com>
2021-08-08 11:13:25 -0400

    base & cursor setup

---
+++ Main/Base.lua
@@ -0,0 +1,13 @@
+AllControls = {}
+
+player = game.Players.LocalPlayer
+mouse = player:GetMouse()
+RunService = game:GetService("RunService")
+
+function DUI.Draw(itemname)
+    local item = Drawing.new(itemname)
+	table.insert(AllControls, item)
+ 	return item
+end
+
+DUI.Load("Main/Cursor")
