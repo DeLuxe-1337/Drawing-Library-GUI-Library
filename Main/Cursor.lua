b02a176b4d39b4e2f0081c73c81f422aceae2ca2
= <74394136+DeLuxe1337001@users.noreply.github.com>
2021-08-08 17:13:44 -0400

    Changed from a square to a circle

--- Main/Cursor.lua
+++ Main/Cursor.lua
@@ -1,14 +1,13 @@
 print("Cursor loaded")
 
-local Cursor = Drawing.new("Square") --Will make a triangle later
-Cursor.Filled = true
-Cursor.Size = Vector2.new(10,10)
+local Cursor = Drawing.new("Circle")
+Cursor.Filled = false
+Cursor.Radius = 1
 Cursor.Visible = true
-Cursor.Color = Color3.fromRGB(85, 85, 85)
-Cursor.Position = Vector2.new(mouse.X, mouse.Y)
+Cursor.Color = Color3.fromRGB(52, 137, 235)
 
 getgenv().Cursor = Cursor
 
-RS.RenderStepped:Connect(function(step) --Just for now gonna change later
-    Cursor.Position = Vector2.new(mouse.X, mouse.Y)
+RS.RenderStepped:Connect(function(step)
+    Cursor.Position = Vector2.new(mouse.X + 3, mouse.Y + 35)
 end)
