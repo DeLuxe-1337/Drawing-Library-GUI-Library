ce0da6833d28fd529493c89cd0ac6d02760d4b51
= <74394136+DeLuxe1337001@users.noreply.github.com>
2021-08-08 17:13:03 -0400

    Changed cursor from a square to a circle

--- Loader.lua
+++ Loader.lua
@@ -6,6 +6,8 @@ function DUI.load(name)
     return loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/DeLuxe1337001/Drawing-Library-GUI-Library/master/" .. name .. ".lua"))()
 end
 
-getgenv().DUI = DUI
+local genv = assert(getgenv, "oop")
+
+genv.DUI = DUI
 
 DUI.load("Main/Base")
